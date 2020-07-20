c407d0f894b0c56fba4bef20a0f3f88feb92b201
John Gile <me@jgile.com>
2020-07-20 09:52:19 -0700

    feat: wip

--- tests/writeAppends.spec.js
+++ tests/writeAppends.spec.js
@@ -1,6 +1,1 @@
-const fs = require('fs');
-const path = require('path');
-const writeAppends = require('../src/functions/templates/writeAppends');
-const _ = require('lodash');
-
 describe('writeAppends', () => {});
